a7d74a85c40f610ecf79fbc268ae39130a299462
dgw <ds257873@naver.com>
2022-11-09 03:06:06 +0900

    Domain

---
+++ src/main/java/com/example/demo/domain/Member.java
@@ -0,0 +1,22 @@
+package com.example.demo.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Member {
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+}
--- src/test/java/com/example/demo/MemberRepositoryTest.java
+++ src/test/java/com/example/demo/MemberRepositoryTest.java
@@ -1,17 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.domain.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.transaction.Transactional;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
